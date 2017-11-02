afdd3bf2e7eca0b4a64ee1501c3b2fd248b03a73
khs1994 <khs1994@khs1994.com>
2017-11-02 10:18:37 +0800

    Update

--- ci.sh
+++ ci.sh
@@ -2,7 +2,19 @@
 
 case $1 in
   ci )
+    cd webhooks
+    if [ ! -d "node_modules" ];then
+       npm install
+    elif [ ! -f ".env" ];then
+       cp .env.example .env
+       cp update.example.js update.js
+       cp update.example.sh update.sh
+    fi
+    echo "修改端口号，密钥之后在 webhooks/ 执行 docker-compose up -d"
   ;;
   * )
   echo -e "khs1994-docker/ci Powered By Gogs & Drone & Registry
+  ci Start webhooks server
   "
+  ;;
+esac
