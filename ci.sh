d77cc85ad83a79dd35e54cc6995da1e35af8ebcb
khs1994 <khs1994@khs1994.com>
2017-11-10 22:45:34 +0800

    Update

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if [ ! -f .env ];then cp .emv.example .env; fi
+if [ ! -f .env ];then cp .env.example .env; fi
 if [ ! -f app.prod.ini ];then cp gogs/app.ini gogs/app.prod.ini; fi
 
 cd webhooks
