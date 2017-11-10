723657de76b36e2ae5b43e65148b1b60f34f7cb7
khs1994 <khs1994@khs1994.com>
2017-11-10 22:33:41 +0800

    Update

--- ci.sh
+++ ci.sh
@@ -5,15 +5,13 @@ if [ ! -f app.prod.ini ];then cp gogs/app.ini gogs/app.prod.ini; fi
 
 cd webhooks
 
-if [ ! -d "node_modules" ];then
-       docker build -t test .
-       docker run test --name=test sh
-       docker cp test:/data/lnmp/ci/node_modules .
-elif [ ! -f ".env" ];then
-       cp .env.example .env
-       cp update.example.js update.js
-       cp update.example.sh update.sh
-fi
+if [ ! -f ".env" ];then cp .env.example .env; cp update.example.js update.js; cp update.example.sh update.sh; fi
+
+# docker build -t khs1994/ci .
+# docker run -it -d \
+#   --name=ci \
+#   -p 1234:7777 \
+#   khs1994/ci
 
 echo -e "修改端口号，密钥之后在 ./webhooks 执行\n
 $ docker-compose up -d\n"
