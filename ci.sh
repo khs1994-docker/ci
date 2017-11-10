2cba55621df588687477171cf87792abcb057fce
khs1994 <khs1994@khs1994.com>
2017-11-10 22:07:28 +0800

    Update

--- ci.sh
+++ ci.sh
@@ -1,21 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-case $1 in
-  ci )
-    cd webhooks
-    if [ ! -d "node_modules" ];then
-       npm install
-    elif [ ! -f ".env" ];then
+if [ ! -f .env ];then cp .emv.example .env; fi
+if [ ! -f app.prod.ini ];then cp gogs/app.ini gogs/app.prod.ini; fi
+
+cd webhooks
+
+if [ ! -d "node_modules" ];then
+       docker build -t test .
+       docker run test --name=test sh
+       docker cp test:/data/lnmp/ci/node_modules .
+elif [ ! -f ".env" ];then
        cp .env.example .env
        cp update.example.js update.js
        cp update.example.sh update.sh
-    fi
-    echo -e "修改端口号，密钥之后在 ./webhooks 执行\n
+fi
+
+echo -e "修改端口号，密钥之后在 ./webhooks 执行\n
 $ docker-compose up -d\n"
-  ;;
-  * )
-  echo -e "khs1994-docker/ci Powered By Gogs & Drone & Registry
-  ci Start webhooks server
-  "
-  ;;
-esac
