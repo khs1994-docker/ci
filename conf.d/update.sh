3c42c6e6e0f188e4b703ab060d5fa4bbb7d96746
khs1994 <khs1994@khs1994.com>
2017-12-14 16:10:42 +0800

    Update nginx *.conf

--- conf.d/update.sh
+++ conf.d/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. ../.env
+
 BASE_DIR=~/khs1994-lnmp/config/nginx/
 
 rm -rf ${BASE_DIR}drone.conf \
@@ -10,3 +12,19 @@ rm -rf ${BASE_DIR}drone.conf \
 cp -a *.conf ${BASE_DIR}
 cp -a ssl/. ${BASE_DIR}/ssl
 cp -a auth/. ${BASE_DIR}/auth
+
+if [ `uname -s` = "Darwin" ];then
+
+  sed -i "" "s/gogs/${DRONE_IP}/g" ${BASE_DIR}gogs.conf
+
+  sed -i "" "s/drone-server/${DRONE_IP}/g" ${BASE_DIR}drone.conf
+
+  sed -i "" "s/registry-server/${DRONE_IP}/g" ${BASE_DIR}docker-registry.conf
+
+elif [ `uname -s` = "Linux"  ];then
+  sed -i "s/gogs/${DRONE_IP}/g" ${BASE_DIR}gogs.conf
+
+  sed -i "s/drone-server/${DRONE_IP}/g" ${BASE_DIR}drone.conf
+
+  sed -i "s/registry-server/${DRONE_IP}/g" ${BASE_DIR}docker-registry.conf
+fi
